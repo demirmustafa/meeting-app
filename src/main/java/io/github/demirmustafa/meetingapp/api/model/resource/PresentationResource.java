a1feb00b8b61ce67e9e9ec5a10c60a6dbe28d570
Mustafa Demir <mustafa.demir@b2cdirect.com>
2019-10-07 14:30:03 +0300

    created PresentationResource

---
+++ src/main/java/io/github/demirmustafa/meetingapp/api/model/resource/PresentationResource.java
@@ -0,0 +1,19 @@
+package io.github.demirmustafa.meetingapp.api.model.resource;
+
+import io.github.demirmustafa.meetingapp.domain.enums.PresentationTimeType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PresentationResource {
+
+    private String name;
+    private Integer minutes;
+    private PresentationTimeType type;
+    private SpeakerResource speaker;
+}
--- src/main/java/io/github/demirmustafa/meetingapp/mapper/PresentationResponseMapper.java
+++ src/main/java/io/github/demirmustafa/meetingapp/mapper/PresentationResponseMapper.java
@@ -1,5 +1,7 @@
 package io.github.demirmustafa.meetingapp.mapper;
 
+import io.github.demirmustafa.meetingapp.api.model.resource.PresentationResource;
+import io.github.demirmustafa.meetingapp.api.model.resource.SpeakerResource;
 import io.github.demirmustafa.meetingapp.api.model.response.CreatePresentationResponse;
 import io.github.demirmustafa.meetingapp.domain.entity.Presentation;
 import org.springframework.stereotype.Component;
@@ -10,6 +12,15 @@ public class PresentationResponseMapper {
     public CreatePresentationResponse entity2CreateResponse(Presentation entity) {
         return CreatePresentationResponse.builder()
                 .id(entity.getId())
+                .build();
+    }
+
+    public PresentationResource entity2Resource(Presentation entity, SpeakerResource speaker) {
+        return PresentationResource.builder()
+                .name(entity.getName())
+                .minutes(entity.getMinutes())
+                .type(entity.getPresentationTimeType())
+                .speaker(speaker)
                 .build();
     }
 }
