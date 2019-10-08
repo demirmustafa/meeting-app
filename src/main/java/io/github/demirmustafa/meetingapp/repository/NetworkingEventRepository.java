e74b052ad6646117586f78ecceb870a87cd1c0eb
Mustafa Demir <mustafa.demir@b2cdirect.com>
2019-10-08 10:31:13 +0300

    deleted networking event

--- src/main/java/io/github/demirmustafa/meetingapp/repository/NetworkingEventRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.demirmustafa.meetingapp.repository;
-
-import io.github.demirmustafa.meetingapp.domain.entity.NetworkingEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NetworkingEventRepository extends JpaRepository<NetworkingEvent, Long> {
-}
