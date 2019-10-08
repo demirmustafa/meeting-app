b37955870b2581da6e4c2ea624b8781fb228664a
Mustafa Demir <mustafa.demir@b2cdirect.com>
2019-10-08 23:20:15 +0300

    handle BusinessException on ControllerAdvice

--- src/main/java/io/github/demirmustafa/meetingapp/api/advice/ControllerAdvice.java
+++ src/main/java/io/github/demirmustafa/meetingapp/api/advice/ControllerAdvice.java
@@ -1,13 +1,13 @@
 package io.github.demirmustafa.meetingapp.api.advice;
 
+import io.github.demirmustafa.meetingapp.Messages;
 import io.github.demirmustafa.meetingapp.api.MeetingApi;
+import io.github.demirmustafa.meetingapp.exception.BusinessException;
 import io.github.demirmustafa.meetingapp.validation.exception.ModelNotValidException;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,19 +16,29 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice(annotations = {
         MeetingApi.class
 })
+@RequiredArgsConstructor
 public class ControllerAdvice {
+
+    private final Messages messages;
 
     @ExceptionHandler(ModelNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handle(ModelNotValidException e) {
         log.error("[ControllerAdvice] handles ModelNotValidException", e);
-        return anErrorResponse(HttpStatus.BAD_REQUEST, e);
+        return anErrorResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
-    private ErrorResponse anErrorResponse(HttpStatus status, Throwable e) {
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity handle(BusinessException e) {
+        log.error("[ControllerAdvice] handles BusinessException", e);
+        ErrorResponse response = anErrorResponse(e.getFaultCode().getStatus(), messages.getMessage(e.getFaultCode().getCode()));
+        return new ResponseEntity(response, e.getFaultCode().getStatus());
+    }
+
+    private ErrorResponse anErrorResponse(HttpStatus status, String message) {
         return ErrorResponse.builder()
                 .status(status.value())
-                .message(e.getMessage())
+                .message(message)
                 .build();
     }
 
