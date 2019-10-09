2868607bd28cec630a21e2f1881466e747d892c8
Mustafa Demir <mustafa.demir@b2cdirect.com>
2019-10-10 00:41:19 +0300

    get all presentations impl

--- src/main/java/io/github/demirmustafa/meetingapp/api/controller/PresentationController.java
+++ src/main/java/io/github/demirmustafa/meetingapp/api/controller/PresentationController.java
@@ -2,6 +2,7 @@ package io.github.demirmustafa.meetingapp.api.controller;
 
 import io.github.demirmustafa.meetingapp.api.MeetingApi;
 import io.github.demirmustafa.meetingapp.api.model.request.CreatePresentationRequest;
+import io.github.demirmustafa.meetingapp.api.model.resource.PresentationResource;
 import io.github.demirmustafa.meetingapp.api.model.response.CreatePresentationResponse;
 import io.github.demirmustafa.meetingapp.api.validator.CreatePresentationRequestValidator;
 import io.github.demirmustafa.meetingapp.service.PresentationService;
@@ -9,9 +10,12 @@ import io.github.demirmustafa.meetingapp.validation.annotation.Valid;
 import io.github.demirmustafa.meetingapp.validation.annotation.Validator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
 
 @MeetingApi
 @RequiredArgsConstructor
@@ -24,5 +28,10 @@ public class PresentationController {
     @Validator(validator = CreatePresentationRequestValidator.class)
     public CreatePresentationResponse create(@Valid @RequestBody CreatePresentationRequest request) {
         return presentationService.create(request);
+    }
+
+    @GetMapping("/presentations")
+    public List<PresentationResource> getAll() {
+        return presentationService.getAll();
     }
 }
--- src/main/java/io/github/demirmustafa/meetingapp/service/PresentationService.java
+++ src/main/java/io/github/demirmustafa/meetingapp/service/PresentationService.java
@@ -1,17 +1,24 @@
 package io.github.demirmustafa.meetingapp.service;
 
 import io.github.demirmustafa.meetingapp.api.model.request.CreatePresentationRequest;
+import io.github.demirmustafa.meetingapp.api.model.resource.PresentationResource;
+import io.github.demirmustafa.meetingapp.api.model.resource.SpeakerResource;
 import io.github.demirmustafa.meetingapp.api.model.response.CreatePresentationResponse;
 import io.github.demirmustafa.meetingapp.domain.entity.Presentation;
 import io.github.demirmustafa.meetingapp.domain.entity.Speaker;
 import io.github.demirmustafa.meetingapp.exception.BusinessException;
 import io.github.demirmustafa.meetingapp.exception.FaultCode;
 import io.github.demirmustafa.meetingapp.mapper.PresentationResponseMapper;
+import io.github.demirmustafa.meetingapp.mapper.SpeakerResponseMapper;
 import io.github.demirmustafa.meetingapp.repository.PresentationRepository;
 import io.github.demirmustafa.meetingapp.repository.SpeakerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -21,11 +28,21 @@ public class PresentationService {
     private final PresentationRepository presentationRepository;
     private final SpeakerRepository speakerRepository;
     private final PresentationResponseMapper presentationResponseMapper;
+    private final SpeakerResponseMapper speakerResponseMapper;
 
     public CreatePresentationResponse create(CreatePresentationRequest request) {
         Speaker speaker = speakerRepository.findById(request.getSpeakerId()).orElseThrow(() -> new BusinessException(FaultCode.PRESENTATION_SPEAKER_NOT_FOUND));
         Presentation presentation = new Presentation(request, speaker);
         Presentation saved = presentationRepository.save(presentation);
         return presentationResponseMapper.entity2CreateResponse(saved);
+    }
+
+    public List<PresentationResource> getAll() {
+        Function<Speaker, SpeakerResource> mapToResource = speakerResponseMapper::entity2Resource;
+
+        return presentationRepository.findAll()
+                .stream()
+                .map(presentation -> presentationResponseMapper.entity2Resource(presentation, mapToResource.apply(presentation.getSpeaker())))
+                .collect(Collectors.toList());
     }
 }
